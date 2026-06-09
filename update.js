bfa4cad61993827604f0bd3f19d1999ef827a322
cyhub1122 <1838555724@qq.com>
2026-06-09 21:20:09 +0800

    Add files via upload

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 _jsonpNoticeCb({
     "title": "初忆AI助手公告",
-    "content": "欢迎使用初忆AI助手！<br><br>网页版：<a href=\"https://preview.yybappbuilder.qq.com/1779639959407c7f49371b3978075/250#/\" target=\"_blank\" rel=\"noopener noreferrer\">点我跳转</a><br>发布/更新：<a href=\"https://1812155487.share.123865.com/123pan/BYDrVv-kyKAd\" target=\"_blank\" rel=\"noopener noreferrer\">点我跳转</a><br><br>新版本持续优化对话逻辑、模型响应速度，修复已知bug，建议定期前往更新。<br>如有问题或建议可前往设置→关于底部邮箱反馈。"
+    "content": "欢迎使用初忆AI助手！<br><br>开发人员名单：<a href=\"https://qm.qq.com/q/7YdRTP1M3u\" target=\"_blank\" rel=\"noopener noreferrer\">点我跳转</a><br>发布/更新：<a href=\"https://1812155487.share.123865.com/123pan/BYDrVv-kyKAd\" target=\"_blank\" rel=\"noopener noreferrer\">点我跳转</a><br><br>新版本持续优化对话逻辑、模型响应速度，修复已知bug，建议定期前往更新。<br>如有问题或建议可前往设置→关于底部邮箱反馈。"
 });
